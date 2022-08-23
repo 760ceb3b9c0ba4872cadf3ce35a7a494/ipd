f1b38b01348ab2cf3ad5a4f3819e7fe24c6bb559
760ceb3b9c0ba4872cadf3ce35a7a494 <109923290+760ceb3b9c0ba4872cadf3ce35a7a494@users.noreply.github.com>
2022-08-23 14:19:25 -0400

    Update ipd_file.py

--- ipd_file.py
+++ ipd_file.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass
 from typing import BinaryIO, Tuple, List, Optional
 
 from PIL import Image, ImagePalette
-from PIL.Image import FLIP_TOP_BOTTOM
 
 
 def pixel_from565(pixel: int) -> Tuple[int, int, int]:
@@ -15,9 +15,9 @@ def pixel_from565(pixel: int) -> Tuple[int, int, int]:
 def pixel_to565(pixel: Tuple[int, int, int]) -> int:
     return (
             ((
-                    (int(0b11111 * (int(pixel[0]) / 255)) << 6) +
-                    (int(0b111111 * (int(pixel[1]) / 255)))
-            ) << 5) +
+                     (int(0b11111 * (int(pixel[0]) / 255)) << 6) +
+                     (int(0b111111 * (int(pixel[1]) / 255)))
+             ) << 5) +
             (int(0b11111 * (int(pixel[2]) / 255)))
     )
 
@@ -34,9 +34,9 @@ def pixel_to556(pixel: Tuple[int, int, int]) -> int:
     # I hate myself
     return (
             ((
-                    (int(0b11111 * (int(pixel[0]) / 255)) << 5) +
-                    (int(0b11111 * (int(pixel[1]) / 255)))
-            ) << 6) +
+                     (int(0b11111 * (int(pixel[0]) / 255)) << 5) +
+                     (int(0b11111 * (int(pixel[1]) / 255)))
+             ) << 6) +
             (int(0b111111 * (int(pixel[2]) / 255)))
     )
 
@@ -74,16 +74,46 @@ def pixels_from565(stream: BinaryIO, length: int) -> List[Tuple[int, int, int]]:
     return pixels_list
 
 
+@dataclass
+class IPDHeader:
+    width: int
+    height: int
+    flags: int
+    bits_per_pixel: int
+
+
+def read_header(stream: BinaryIO):
+    width_b = stream.read(4)
+    height_b = stream.read(4)
+    flags_b = stream.read(1)
+    color_depth_b = stream.read(1)
+    blank_b = stream.read(6)
+
+    if (
+            # fixme! hack alert
+            len(width_b) != 4 or
+            len(height_b) != 4 or
+            len(flags_b) != 1 or
+            len(color_depth_b) != 1 or
+            len(blank_b) != 6
+    ):
+        raise ValueError("not enough header data")
+
+    return IPDHeader(
+        int.from_bytes(width_b, "little"),
+        int.from_bytes(height_b, "little"),
+        int.from_bytes(flags_b, "little"),
+        int.from_bytes(color_depth_b, "little")
+    )
+
+
 def from_stream(
         stream: BinaryIO,
         *,
         force_flip: Optional[bool] = None
-):
-    width = int.from_bytes(stream.read(4), "little")
-    height = int.from_bytes(stream.read(4), "little")
-    flags = int.from_bytes(stream.read(1), "little")
-    color_depth = int.from_bytes(stream.read(1), "little")
-    stream.seek(6, 1)
+) -> Tuple[IPDHeader, Image.Image]:
+    header = read_header(stream)
+    width, height, flags, color_depth = header.width, header.height, header.flags, header.bits_per_pixel
 
     extra_palette = None
     flip_v = False
@@ -104,8 +134,11 @@ def from_stream(
 
         image = Image.new("RGB", (width, height))
         image.putdata(pixels_from556(stream, (width * height) * 2))
-        image = image.transpose(FLIP_TOP_BOTTOM)
-        return image
+
+        if (force_flip is None) or force_flip:
+            image = image.transpose(Image.FLIP_TOP_BOTTOM)
+
+        return header, image
     elif flags == 0b00001010:
         img_mode = "P"
         decoder_mode = "P"
@@ -135,14 +168,20 @@ def from_stream(
         image = Image.new("RGB", (width, height))
         image.putdata(pixels_from565(stream, (width * height) * 2))
 
-        return image
+        if force_flip:
+            image = image.transpose(Image.FLIP_TOP_BOTTOM)
+
+        return header, image
     elif flags == 0b00000011:
         img_mode = "L"
         decoder_mode = "L;16"
     else:
-        raise NotImplementedError("img not implemented")
+        raise NotImplementedError(f"flag {flags:08b} not implemented")
 
-    image_data = stream.read(int((width * height) * (color_depth / 8)))
+    data_length = int((width * height) * (color_depth / 8))
+    image_data = stream.read(data_length)
+    if len(image_data) != data_length:
+        raise ValueError(f"not enough image data! needed {data_length} bytes, got {len(image_data)}")
 
     image = Image.frombytes(
         img_mode,
@@ -151,12 +190,11 @@ def from_stream(
         "raw",
         decoder_mode,
         0,
-        (-1 if flip_v else 1) if force_flip is None else force_flip  # orientation
+        (-1 if flip_v else 1) if force_flip is None else
+        (-1 if force_flip else 1)
     )
 
     if extra_palette:
         image.putpalette(extra_palette)
 
-    return image
-
-
+    return header, image
