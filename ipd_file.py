460323bd5dbed17cdac0322bda440709737ca5e5
760ceb3b9c0ba4872cadf3ce35a7a494 <109923290+760ceb3b9c0ba4872cadf3ce35a7a494@users.noreply.github.com>
2023-08-13 23:43:59 -0400

    refactor, flags -> mode

--- ipd_file.py
+++ ipd_file.py
@@ -4,6 +4,7 @@ from typing import BinaryIO, Tuple, List, Optional
 from PIL import Image, ImagePalette
 
 
+# pixel formats
 def pixel_from565(pixel: int) -> Tuple[int, int, int]:
     return (
         int((pixel >> 11 & 0b11111) * (255 / 0b11111)),
@@ -31,7 +32,6 @@ def pixel_from556(pixel: int) -> Tuple[int, int, int]:
 
 
 def pixel_to556(pixel: Tuple[int, int, int]) -> int:
-    # I hate myself
     return (
             ((
                      (int(0b11111 * (int(pixel[0]) / 255)) << 5) +
@@ -39,6 +39,7 @@ def pixel_to556(pixel: Tuple[int, int, int]) -> int:
              ) << 6) +
             (int(0b111111 * (int(pixel[2]) / 255)))
     )
+
 
 def pixel_from4444(pixel: int) -> Tuple[int, int, int, int]:
     return (
@@ -48,6 +49,8 @@ def pixel_from4444(pixel: int) -> Tuple[int, int, int, int]:
         int(pixel << 4 & 0b11110000)
     )
 
+
+# pixel/palette level pixel formats
 def palette_from565(stream: BinaryIO, length: int) -> ImagePalette.ImagePalette:
     palette_list = []
 
@@ -90,95 +93,58 @@ def pixels_from4444(stream: BinaryIO, length: int) -> List[Tuple[int, int, int, 
     return pixels_list
 
 
+# header
 @dataclass
 class IPDHeader:
     width: int
     height: int
-    flags: int
+    mode: int
     bits_per_pixel: int
 
 
 def read_header(stream: BinaryIO):
     width_b = stream.read(4)
     height_b = stream.read(4)
-    flags_b = stream.read(1)
+    mode_b = stream.read(1)
     color_depth_b = stream.read(1)
     blank_b = stream.read(6)
 
     if (
-            # fixme! hack alert
-            len(width_b) != 4 or
-            len(height_b) != 4 or
-            len(flags_b) != 1 or
-            len(color_depth_b) != 1 or
-            len(blank_b) != 6
+        # fixme! hack alert
+        len(width_b) != 4 or
+        len(height_b) != 4 or
+        len(mode_b) != 1 or
+        len(color_depth_b) != 1 or
+        len(blank_b) != 6
     ):
         raise ValueError("not enough header data")
 
     return IPDHeader(
         int.from_bytes(width_b, "little"),
         int.from_bytes(height_b, "little"),
-        int.from_bytes(flags_b, "little"),
+        int.from_bytes(mode_b, "little"),
         int.from_bytes(color_depth_b, "little")
     )
 
 
+# stream decode
 def from_stream(
         stream: BinaryIO,
         *,
         force_flip: Optional[bool] = None
 ) -> Tuple[IPDHeader, Image.Image]:
     header = read_header(stream)
-    width, height, flags, color_depth = header.width, header.height, header.flags, header.bits_per_pixel
+    width, height, mode, color_depth = header.width, header.height, header.mode, header.bits_per_pixel
 
     extra_palette = None
     flip_v = False
 
-    if flags == 0b00001000:
-        img_mode = "L"
-        decoder_mode = "L;4"
-        stream.read(32)
-        flip_v = True
-    elif flags == 0b00000010:
-        """
-        
-        This mode is SPECIAL CASED
-        It's 556 ordered RGB data and needs to be flipped
-        This can't be done with the raw mode
-        
-        """
-
-        image = Image.new("RGB", (width, height))
-        image.putdata(pixels_from556(stream, (width * height) * 2))
-
-        if (force_flip is None) or force_flip:
-            image = image.transpose(Image.FLIP_TOP_BOTTOM)
-
-        return header, image
-    elif flags == 0b00001010:
-        img_mode = "P"
-        decoder_mode = "P"
-        extra_palette = palette_from565(stream, 512)
-        # extra_palette = ImagePalette.raw("RGB;16", stream.read(512))
-    elif flags == 0b00000000:
+    if mode == 0x0:
         img_mode = "L"
         decoder_mode = "L"
-    elif flags == 0b00000101:
-        img_mode = "RGBA"
-        decoder_mode = "RGBA"
-        flip_v = True
-    elif flags == 0b00001001:
-        img_mode = "P"
-        decoder_mode = "P"
-        extra_palette = ImagePalette.raw("RGBA", stream.read(1024))
-        flip_v = True
-    elif flags == 0b00000001:
+    elif mode == 0x1:
         """
-
-        This mode is SPECIAL CASED
-        It's **565** ordered RGB data and needs to be flipped
-        This can't be done with the raw mode
-
+        special case: RGB in 565 order. this mode isn't supported by Pillow
         """
 
         image = Image.new("RGB", (width, height))
@@ -188,13 +154,21 @@ def from_stream(
             image = image.transpose(Image.FLIP_TOP_BOTTOM)
 
         return header, image
-    elif flags == 0b00000011:
+    elif mode == 0x2:
         """
+        special case: RGB in 556 order. this mode isn't supported by Pillow
+        """
+        
+        image = Image.new("RGB", (width, height))
+        image.putdata(pixels_from556(stream, (width * height) * 2))
 
-        This mode is SPECIAL CASED
-        It's **4444** ordered RGBA data and needs to be flipped
-        This can't be done with the raw mode
+        if (force_flip is None) or force_flip:
+            image = image.transpose(Image.FLIP_TOP_BOTTOM)
 
+        return header, image
+    elif mode == 0x3:
+        """
+        special case: RGBA in 4444 order. this mode isn't supported by Pillow
         """
         
         image = Image.new("RGBA", (width, height))
@@ -204,8 +178,26 @@ def from_stream(
             image = image.transpose(Image.FLIP_TOP_BOTTOM)
 
         return header, image
+    elif mode == 0x5:
+        img_mode = "RGBA"
+        decoder_mode = "RGBA"
+        flip_v = True
+    elif mode == 0x8:
+        img_mode = "L"
+        decoder_mode = "L;4"
+        stream.read(32)
+        flip_v = True
+    elif mode == 0x9:
+        img_mode = "P"
+        decoder_mode = "P"
+        extra_palette = ImagePalette.raw("RGBA", stream.read(1024))
+        flip_v = True
+    elif mode == 0xA:
+        img_mode = "P"
+        decoder_mode = "P"
+        extra_palette = palette_from565(stream, 512)
     else:
-        raise NotImplementedError(f"flag {flags:08b} not implemented")
+        raise NotImplementedError(f"mode {mode:08b} not implemented")
 
     data_length = int((width * height) * (color_depth / 8))
     image_data = stream.read(data_length)
@@ -219,8 +211,7 @@ def from_stream(
         "raw",
         decoder_mode,
         0,
-        (-1 if flip_v else 1) if force_flip is None else
-        (-1 if force_flip else 1)
+        (-1 if flip_v else 1) if force_flip is None else (-1 if force_flip else 1)
     )
 
     if extra_palette:
